d936a4b858161b29ba60b5328e7f104ff0e5816e
SBS세익 <seik1224@naver.com>
2024-09-13 20:51:41 +0900

    0913수정

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,23 +9,29 @@ const introText = {
 
 const Intro = () => {
     return (
-        <section id="intro">
-            <div className="intro__inner">
+        <section id="intro" className="w-full">
+            <div className="intro__inner w-[1200px] px-10 mx-auto ">
                 <div className="intro__text">
                     <div className="text">
-                        <div>{introText.title[0]}</div>
-                        <div>{introText.title[1]}</div>                      
+                        <div>We Make Games Better</div>
+                        <div>NEXON Games</div>                      
                     </div>
                     <div className="text__p">
                         <p>{introText.desc[0]}</p>
                         <p>{introText.desc[1]}</p>
                     </div>
-                    <div className="img">
-                        <img src={about} alt="어바웃" />
-                    </div>
                 </div>
-              
+                <div className="img absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                    <img src={about} alt="어바웃" />
+                </div>
             </div>
+            {/* <div className="w-full">
+                <div className="w-[1200px]">
+                    이미지
+                </div>
+                <div>라인</div>
+            </div> */}
+            {/* http://seik1224.dothome.co.kr/lango/index.html */}
         </section>
     );
 };
